a3e360e019d63824f3956eb45b90e2fd3cd7222c
Viviane <vivianerosario.ep@gmail.com>
2024-06-02 16:11:35 -0300

    Criação da model order e user e também a criação do enum orderStatus

---
+++ src/main/java/com/vcr/dscommerce/model/OrderModel.java
@@ -0,0 +1,35 @@
+package com.vcr.dscommerce.model;
+
+import java.time.Instant;
+
+import org.hibernate.annotations.ManyToAny;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
+ 
+@Data
+@Entity
+@Table(name = "tb_order")
+public class OrderModel {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant moment;
+
+    private OrderStatusEnum status;
+
+    @ManyToOne
+    @JoinColumn(name = "client_id")
+    private UserModel client;
+
+}
---
+++ src/main/java/com/vcr/dscommerce/model/OrderStatusEnum.java
@@ -0,0 +1,10 @@
+package com.vcr.dscommerce.model;
+
+public enum OrderStatusEnum {
+    
+    WAITING_PAYMENT,
+    PAID,
+    SHIPPED,
+    DELIVERED,
+    CANCELED;
+}

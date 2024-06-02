52b44b590bf32c31be35dd4bbbae7da90e864577
Viviane <vivianerosario.ep@gmail.com>
2024-06-02 16:45:04 -0300

    Payments

--- src/main/java/com/vcr/dscommerce/model/PaymentModel.java
+++ src/main/java/com/vcr/dscommerce/model/PaymentModel.java
@@ -2,22 +2,20 @@ package com.vcr.dscommerce.model;
 
 import java.time.Instant;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
- 
+
 @Data
 @Entity
-@Table(name = "tb_order")
-public class OrderModel {
+@Table(name = "tb_payment")
+public class PaymentModel {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,13 +24,7 @@ public class OrderModel {
     @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant moment;
 
-    private OrderStatusEnum status;
-
-    @ManyToOne
-    @JoinColumn(name = "client_id")
-    private UserModel client;
-
-    @OneToOne(mappedBy = "order", cascade =  CascadeType.ALL)
-     private PaymentModel payment;
-
+    @OneToOne
+    @MapsId
+    private OrderModel order;
 }

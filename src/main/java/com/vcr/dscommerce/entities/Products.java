2210934e9802412547c8745f63979658f850cf6d
Viviane <vivianerosario.ep@gmail.com>
2025-06-02 20:51:33 -0300

    Creation of Category and Products entities - many-to-many relationship

---
+++ src/main/java/com/vcr/dscommerce/entities/Products.java
@@ -0,0 +1,81 @@
+package com.vcr.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_products")
+public class Products {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private Double price;
+
+    private String imgUrl;
+
+    private Set<Category> categories = new HashSet<>();
+
+    public Products(){
+
+    }
+
+    public Products(String description, Long id, String imgUrl, String name, Double price) {
+        this.description = description;
+        this.id = id;
+        this.imgUrl = imgUrl;
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+}

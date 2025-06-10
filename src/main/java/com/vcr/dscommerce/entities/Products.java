92d534b1c1e1764ecda42be66268aa9cc3c6ef15
Viviane <vivianerosario.ep@gmail.com>
2025-06-10 20:40:57 -0300

    Equals hashcode

--- src/main/java/com/vcr/dscommerce/entities/Products.java
+++ src/main/java/com/vcr/dscommerce/entities/Products.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
@@ -95,5 +96,18 @@ public class Products {
 
     public List<Order> getOrders(){
         return items.stream().map(x -> x.getOrder()).toList();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Products products = (Products) o;
+        return Objects.equals(id, products.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
 }
